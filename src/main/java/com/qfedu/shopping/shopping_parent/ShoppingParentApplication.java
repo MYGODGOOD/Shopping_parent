aea7b8ceefcb9bd3fdfdc12dc4ee04df1cb7a1e8
root <2313583800@qq.com>
2019-06-22 09:41:07 +0800

    初始版本

---
+++ src/main/java/com/qfedu/shopping/shopping_parent/ShoppingParentApplication.java
@@ -0,0 +1,13 @@
+package com.qfedu.shopping.shopping_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingParentApplication.class, args);
+    }
+
+}
